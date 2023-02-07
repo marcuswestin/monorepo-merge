6b59e448b4964398dc690fb7aa08b87b865fb47d
Marcus Westin <marcus.westin@gmail.com>
2023-02-07 11:46:02 -0500

    Update script to do it one repo at a time, and add usage/help instructions

--- import-repos-into-monorepo.sh
+++ import-repos-into-monorepo.sh
@@ -1,128 +1,117 @@
 #!/bin/bash
-#
-# Create a new monorepo by fusing multiple repositories.
-#
+
+# Input checks and help/usage
+#############################
+
+IMPORT_REPO_URL="$1"
+MONOREPO_URL="$2"
+IMPORT_TO_FOLDER="$3"
+
+print_usage() {
+    echo
+    echo "  Import a repo into your monorepo."
+    echo
+    echo "  Usage:"
+    echo
+    echo "      $0 <REPO_URL> <MONOREPO_URL> <FOLDER>"
+    echo
+    echo "  Example usage:"
+    echo
+    echo "      $0 \\
+                    https://github.com/marcuswestin/store.js.git \\
+                    https://github.com/my_account/my_monorepo \\
+                    js/imports/store"
+    echo
+}
+
+if [[ ( $@ == "--help") ||  $@ == "-h" ]]; then
+    print_usage
+    exit 0
+fi
+
+if [[ ${MONOREPO_URL} == "" || ${IMPORT_REPO_URL} == '' || ${IMPORT_TO_FOLDER} == '' ]]; then
+    print_usage
+    echo
+    echo "  Error: missing arguments!"
+    echo
+    exit -1
+fi
+
+# Step 0: Setup
+###############
+
+IMPORT_REPO_FOLDER=$(basename -- "${IMPORT_REPO_URL}")
+IMPORT_REPO_NAME="${IMPORT_REPO_FOLDER%.*}"
+
+MONOREPO_BASENAME=$(basename -- "${MONOREPO_URL}")
+MONOREPO_NAME="${MONOREPO_BASENAME%.*}"
+MONOREPO_EXTENSION="${MONOREPO_BASENAME##*.}"
+MONOREPO_FOLDER="${MONOREPO_NAME}-monorepo-import.${MONOREPO_EXTENSION}"
+
+git clone ${MONOREPO_URL} ${MONOREPO_BASENAME} || echo "already cloned"
+cd ${MONOREPO_BASENAME}
+
+# Uncomment this to start from scratch
+# rm -rf "${MONOREPO_FOLDER}"
+
+
+# Phase 1: Fetch remote for repo to import
+##########################################
+
 # Exit immediately if a command exits with a non-zero status,
 # and print commands and their arguments as they are executed.
 set -ex
 
-# Set the name of the org, and the monorepo to create
-GITHUB_ORG="github-org-or-account"
-MONOREPO_NAME="test-monorepo"
-
-if [ -d "${MONOREPO_NAME}" ]; then
-    echo "Directory ${MONOREPO_NAME} already exists!"
-    exit -1 # Can be commented out to try multiple times
-    rm -rf "${MONOREPO_NAME}"
-fi
-
-# Create the monorepo folder, init, and make a first
-# commit (which is needed before we can merge in repos).
-########################################################
-
-mkdir ${MONOREPO_NAME}
-cd ${MONOREPO_NAME}
-git init
-
-echo "this-will-be-removed" > this-will-be-removed
-git add this-will-be-removed
-git commit -m "this-will-be-removed"
+git remote add "${IMPORT_REPO_NAME}" "${IMPORT_REPO_URL}" || echo "Remote already exists"
+git fetch "${IMPORT_REPO_NAME}"
 
 
-# Phase 1: Setup all repos to be imported
-#########################################
 
-function setup_repos() {
-    TARGET_FOLDER="$1"
-    IMPORT_REPOS="$2"
+# Phase 2: Import repo into a seperate branch and rewrite its
+# commit history to fit in with the monorepo folder structure.
+##############################################################
 
-    for REPO in ${IMPORT_REPOS}; do
-        git remote add "${REPO}" "https://github.com/${GITHUB_ORG}/${REPO}.git"
-    done
-}
+git checkout -f -b "${IMPORT_REPO_NAME}_master" "${IMPORT_REPO_NAME}/master"
+# Rewrite history to move all IMPORT_REPO_NAME files into a subdirectory
+FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f --index-filter '\
+    git ls-files -s | sed "s#\t#&'"${IMPORT_TO_FOLDER}"'/#g" \
+        | GIT_INDEX_FILE=${GIT_INDEX_FILE}.new git update-index --index-info && \
+            if [ -f "${GIT_INDEX_FILE}.new" ]; then \
+                mv "${GIT_INDEX_FILE}.new" "${GIT_INDEX_FILE}"; \
+            fi' --
 
-# Phase 2: Import each repos into a seperate branch and then rewrite
-# their commit history to fit in with the monorepo folder structure.
-####################################################################
 
-function rewrite_history() {
-    TARGET_FOLDER="$1"
-    IMPORT_REPOS="$2"
+# Phase 3: Merge repo and its rewritten history into the monorepo
+#################################################################
 
-    # Checkout all the master branches of the child repositories
-    for REPO in ${IMPORT_REPOS}; do
-            git checkout -f -b "${REPO}_master" "${REPO}/master"
-            # Rewrite history to move all REPO files into a subdirectory
-            export SUBDIRECTORY="${TARGET_FOLDER}/${REPO}"
-            FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f --index-filter '\
-                git ls-files -s | sed "s#\t#&'"${SUBDIRECTORY}"'/#g" \
-                    | GIT_INDEX_FILE=${GIT_INDEX_FILE}.new git update-index --index-info && \
-                        if [ -f "${GIT_INDEX_FILE}.new" ]; then \
-                            mv "${GIT_INDEX_FILE}.new" "${GIT_INDEX_FILE}"; \
-                        fi' --
-    done
-}
+git checkout master
 
-# Phase 3: Merge all repos and their rewritten histories into the monorepo
-##########################################################################
+git merge --no-commit --allow-unrelated-histories "${IMPORT_REPO_NAME}_master"
 
-function merge_repos() {
-    TARGET_FOLDER="$1"
-    IMPORT_REPOS="$2"
+git commit -a -m "Import ${IMPORT_REPO_URL} to ${IMPORT_TO_FOLDER}" || echo "Nothing to do"
 
-    # Merge all the repositories in our master branch.
-    for REPO in ${IMPORT_REPOS}; do
-        git merge --no-commit --allow-unrelated-histories "${REPO}_master"
-        git commit -a -m \
-            "Import ${REPO} from https://github.com/${GITHUB_ORG}/${REPO}.git to ${TARGET_FOLDER}/${REPO}" \
-            || echo "Nothing to do"
-    done
+git branch -D "${IMPORT_REPO_NAME}_master"
+git remote remove "${IMPORT_REPO_NAME}"
 
-    # remove all child REPO branches and remotes
-    for REPO in ${IMPORT_REPOS}; do
-        git branch -D "${REPO}_master"
-        git remote remove "${REPO}"
-    done
-}
 
-# Execute Phase 1
-setup_repos "apps/ts" "my-ts-app another-ts-app third-ts-app"
-setup_repos "apps/py" "py-server py-app another-py-app"
-setup_repos "apps/tools" "misc-tool-repo another-tool etc-etc-etc"
+# Phase 4: Prune all history and do an aggressive gc
+####################################################
 
-# Execute Phase 2
-git fetch --all
-rewrite_history "apps/ts" "my-ts-app another-ts-app third-ts-app"
-rewrite_history "apps/py" "py-server py-app another-py-app"
-rewrite_history "apps/tools" "misc-tool-repo another-tool etc-etc-etc"
-
-# Execute Phase 3
-git checkout -b master
-merge_repos "apps/ts" "my-ts-app another-ts-app third-ts-app"
-merge_repos "apps/py" "py-server py-app another-py-app"
-merge_repos "apps/tools" "misc-tool-repo another-tool etc-etc-etc"
-
-# Finally, prune all history and do an aggressive gc
 git reflog expire --expire=now --all
 git gc --prune=now --aggressive
 
-# Finish up by creating a new README in the root directory!
-###########################################################
-
-MONOREPO_URL="https://github.com/${GITHUB_ORG}/${MONOREPO_NAME}.git"
-echo "# ${MONOREPO_NAME}" > README.md
-git add README.md
-git commit -m "Finished creating ${MONOREPO_URL}"
-git remote add ${MONOREPO_NAME} ${MONOREPO_URL}
-
-# Print instructions for how to force push the new monorepo.
-############################################################
+# Print instructions for how to push changes to the monorepo.
+#############################################################
 set +x
 
 CWD=`pwd`
 echo
-echo "  Done! Finish up by --force pushing the new repo:"
+echo "  Done!"
+echo
+echo "  ${IMPORT_REPO_URL} was imported to ${MONOREPO_BASENAME}/${IMPORT_TO_FOLDER}."
+echo
+echo "  Finish up by pushing the new repo:"
 echo
 echo "      cd ${CWD}"
-echo "      git push --force ${MONOREPO_NAME} master"
+echo "      git push origin master"
 echo
